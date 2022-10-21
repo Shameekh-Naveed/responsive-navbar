a53b20c52672329265a606d3b1a56a9749e92dc0
Shameekh-Naveed <shameekh2002@gmail.com>
2022-10-21 23:21:12 +0500

    Basic navbar implemented

---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <nav>
+      <span className="" id="logo">
+        <img src="https://img.icons8.com/external-kosonicon-flat-kosonicon/50/000000/external-weather-news-weather-kosonicon-flat-kosonicon-5.png" />
+      </span>
+      <span className="" id="hamburger">
+        <img
+          src="https://img.icons8.com/fluency/32/000000/menu-rounded.png"
+          onClick={() => setActive(!active)}
+        />
+      </span>
+      <ul>
+        {/* <div className="push"> */}
+        <li className={`nav-item ${active ? "active" : ""}`}>
+          <a href="#">Home</a>
+        </li>
+        {/* </div> */}
+        <li className="nav-item">
+          <a href="#">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Login</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
